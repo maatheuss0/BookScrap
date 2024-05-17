5dd6adea25fb1ebad2bdfc4d22ef8b8e15cd41f2
maatheuss0 <82424913+maatheuss0@users.noreply.github.com>
2024-05-17 19:10:58 -0300

    Dados remama

--- bookScrap/Códigos/books.py
+++ bookScrap/Códigos/books.py
@@ -1,64 +1,130 @@
 import time
+from datetime import date
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 service = Service()
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=options)
 
-url = "https://books.toscrape.com/"
+url = "https://www.instagram.com/"
 
 driver.get(url)
-time.sleep(2)
-
-links = driver.find_elements(By.TAG_NAME, value="a")
-print(len(links))
-
-"""
-Os livros comecao no 54 e terminam no 94
+time.sleep(3)
 
 """
 
-x = driver.find_elements(By.TAG_NAME, value="a")[54].text
-print(x)
+Rolagem pagina WEB
 
-y = driver.find_elements(By.TAG_NAME, value="a")[54].get_attribute("title")
-print(y)
+"""
 
-print(driver.find_elements(By.TAG_NAME, value="a")[54:94:2])
+scroll_pause_time = 2  # pausa no tempo do scroll para acompanhar o processo
+screen_height = driver.execute_script("return window.screen.height;")  # pega o tamanho da pagina
+i = 1
+while True:
+    # desce a pagina no tamanho de uma tela por vez
+    driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))
+    i += 1
+    time.sleep(scroll_pause_time)
+    # atualize a altura da rolagem sempre que rolar, pois a altura da rolagem pode mudar depois que rolamos a página
+    scroll_height = driver.execute_script("return document.body.scrollHeight;")
+    # interrompa o loop quando a altura que precisamos rolar for maior que a altura total da rolagem
+    if (screen_height) * i > scroll_height:
+        break
+"""
 
-elementostitulos = driver.find_elements(By.TAG_NAME, value="a")[54:94:2]
+Login
 
-listatitulos = [title.get_attribute("title") for title in elementostitulos]
-print(listatitulos)
+"""
 
-elementostitulos[1].click()
+driver.find_element(By.NAME, "username").send_keys("mamaeubarco")
+driver.find_element(By.NAME, "password").send_keys("passhard123")
+driver.find_element(By.NAME, "password").send_keys(Keys.ENTER)
+time.sleep(7)
+
+"""
+
+Caminho da paginá
+
+"""
+
+pesquisa = driver.find_elements(By.TAG_NAME,value='svg')[2]
+pesquisa.click()
 time.sleep(2)
 
-stok = driver.find_element(By.CLASS_NAME, value="instock").text
+driver.find_element(By.TAG_NAME, "input").send_keys("remama")
+driver.find_element(By.TAG_NAME, "input").send_keys(Keys.ENTER)
 time.sleep(2)
-print(stok)
 
-estoque = int(stok.replace("In stock (","").replace(" available)", ""))
-print(estoque)
-driver.back()
+pesquisar = driver.find_elements(By.TAG_NAME, value="a")[0:15]
+pesquisar[11].click()
+time.sleep(5)
 
-listaestoque = []
+"""
 
-for titulo in elementostitulos:
-    titulo.click()
-    time.sleep(1)
-    qtd = int(driver.find_element(By.CLASS_NAME, value="instock").text.replace("In stock (","").replace(" available)",""))
-    listaestoque.append(qtd)
+Comeco raspagem de dados
+
+"""
+
+"""
+
+Leitura das publicacoes, seguidores e seguindo
+
+"""
+
+indice = driver.find_elements(By.CLASS_NAME, value="html-span")[0:3]
+
+indiceDicio = {"Data": date.today().strftime('%d/%m/%Y')
+    , "Publicacoes": indice[0].text
+    , "Seguidores": indice[1].text
+    , "Seguindo": indice[2].text
+               }
+print(indiceDicio)
+
+quantidade = int(indice[0].text)
+
+"""
+
+Leitura das curtidas e data das publicacoes
+
+"""
+
+publicacao = driver.find_elements(By.CLASS_NAME, value="_aagw")[0:quantidade]
+
+curtidaLista = []
+dataLista = []
+
+for p in publicacao:
+
+    p.click()
+    time.sleep(3)
+
+    curtida = driver.find_elements(By.CLASS_NAME, value="html-span")[3:]
+    curtidaPublicacao=(len(curtida))
+
+    data = driver.find_elements(By.CLASS_NAME, value="x1p4m5qa")[0].text
+
+    for c in range(1, curtidaPublicacao):
+        print(data, curtida[c].text)
+        curtidaLista.append(curtida[c].text)
+        dataLista.append(data)
+
     driver.back()
-    time.sleep(1)
+    time.sleep(2)
 
-print(listaestoque)
+"""
 
-data = {"Titulo": listatitulos, "Estoque": listaestoque}
-print(pd.DataFrame(data))
+Exportacao de dados para excel
 
-dados = pd.DataFrame(data)
-dados.to_excel("dados.xlsx")
+"""
+
+#indiceDicio = {"Data da publicacao": dataLista, "Número de curtidas": curtidaLista}
+indiceDicio["Data da publicacao"] = dataLista
+indiceDicio["Número de curtidas"] = curtidaLista
+
+dfexel = pd.DataFrame(indiceDicio)
+
+dfexel.to_excel("dados.xlsx", sheet_name="Dados REMAMA")
